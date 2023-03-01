41f561bfe4540ee6e9356fe002d1a605347d3ef0
Lina Kutitskaya <kutitckaya.lina@yandex.ru>
2023-03-01 10:35:34 +0300

    first commit

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Phone
+
+# Register your models here.
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image', 'price', 'release_date', 'lte_exists', 'slug']
+    list_filter = ['id', 'name', 'price', 'release_date']
+    prepopulated_fields = {'slug': ['name']}
---
+++ views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+
+from phones.models import Phone
+
+
+def index(request):
+    return redirect('catalog')
+
+
+def show_catalog(request):
+    sort = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort == 'name':
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+    template = 'catalog.html'
+
+    context = {
+        'phones': phones
+    }
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    template = 'product.html'
+    phone = Phone.objects.filter(slug=slug)[0]
+    print(phone.name)
+    context = {
+        'phone': phone
+    }
+    return render(request, template, context)
